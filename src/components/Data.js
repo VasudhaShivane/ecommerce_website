6d8f3153d089d87ab88a88c14650276f20e93aa9
Vasudha <shivanevasudha@gmail.com>
2024-04-17 12:24:10 +0530

    hello

---
+++ src/components/Data.js
@@ -0,0 +1,47 @@
+const Data = {
+  productItems: [
+    {
+      id: 1,
+      discount: 50,
+      cover: "./images/flash/flash-1.png",
+      name: "Shoes",
+      price: 3100,
+    },
+    {
+      id: 2,
+      discount: 40,
+      cover: "./images/flash/flash-2.png",
+      name: "Watch",
+      price: 5420,
+    },
+    {
+      id: 3,
+      discount: 40,
+      cover: "./images/flash/flash-3.png",
+      name: "Smart Mobile Black",
+      price: 3200,
+    },
+    {
+      id: 4,
+      discount: 40,
+      cover: "./images/flash/flash-4.png",
+      name: "Smart Watch Black",
+      price: 2350,
+    },
+    {
+      id: 5,
+      discount: 50,
+      cover: "./images/flash/flash-1.png",
+      name: "Shoes",
+      price: 4100,
+    },
+    {
+      id: 6,
+      discount: 50,
+      cover: "./images/flash/flash-3.png",
+      name: "Shoes",
+      price: 5100,
+    },
+  ],
+}
+export default Data
---
+++ src/components/discount/Ddata.js
@@ -0,0 +1,48 @@
+const Ddata = [
+  {
+    cover: "./images/discount/discount-1.png",
+    name: "BenuX 2022",
+    price: "Rs3250",
+  },
+  {
+    cover: "./images/discount/discount-2.png",
+    name: "Sony TV 1080p",
+    price: "Rs4450",
+  },
+  {
+    cover: "./images/discount/discount-3.png",
+    name: "Sony PS4",
+    price: "Rs5056",
+  },
+  {
+    cover: "./images/discount/discount-4.png",
+    name: "Setgearr 2022",
+    price: "Rs5600",
+  },
+  {
+    cover: "./images/discount/discount-5.png",
+    name: "Tony BGB",
+    price: "Rs6720",
+  },
+  {
+    cover: "./images/discount/discount-6.png",
+    name: "RG products",
+    price: "Rs2400",
+  },
+  {
+    cover: "./images/discount/discount-7.png",
+    name: "Ranasonic 2022",
+    price: "Rs3050",
+  },
+  {
+    cover: "./images/discount/discount-8.png",
+    name: "Pune HD",
+    price: "Rs3090",
+  },
+  {
+    cover: "./images/discount/discount-9.png",
+    name: "Sony CCTV",
+    price: "Rs8033",
+  },
+]
+export default Ddata
